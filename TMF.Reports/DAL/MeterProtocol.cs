56ac95e22b6a16f595c3d72c6588edfa93590838
j1rjacob <junarjacob@yahoo.com>
2017-11-08 12:48:00 +0300

    MeterProtocol codes added

--- TMF.Reports/DAL/MeterProtocol.cs
+++ TMF.Reports/DAL/MeterProtocol.cs
@@ -1,12 +1,163 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using TMF.Core;
+using TMF.Core.Model;
 
 namespace TMF.Reports.DAL
 {
-    class MeterProtocol
+    public class MeterProtocol : BaseDAL
     {
+        public MeterProtocol()
+        {
+            SQL_DELETE = "[REPORT METERPROTOCOL_DEL]";
+            SQL_GET = "[REPORT METERPROTOCOL_GET]";
+            SQL_GET_LIST = "[REPORT METERPROTOCOL_LST]";
+            SQL_INSERT = "[REPORT METERPROTOCOL_INS]";
+            SQL_UPDATE = "[REPORT METERPROTOCOL_UPD]";
+            PARM_ID = "@Id";
+        }
+        protected void SetInfo(out Model.MeterProtocol info, SqlDataReader reader)
+        {
+            try
+            {
+                info = new Model.MeterProtocol();
+                info.Id = CastDBNull.To<string>(reader["Id"], "");
+                info.Description = CastDBNull.To<string>(reader["Description"], "");
+                info.CreatedBy = CastDBNull.To<string>(reader["Createdby"], "");
+                info.EditedBy = CastDBNull.To<string>(reader["Editedby"], "");
+                info.DocDate = CastDBNull.To<DateTime>(reader["DocDate"], DateTime.Now);
+                info.Show = CastDBNull.To<int>(reader["Show"], 1);
+                info.LockCount = CastDBNull.To<int>(reader["LockCount"], 1);
+                info.IsNew = false;
+                info.IsDirty = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public override IInfo GetRecord(SmartDB dbInstance, object Id)
+        {
+            string sQL_GET = this.SQL_GET;
+            SqlParameter[] array = new SqlParameter[]
+            {
+                new SqlParameter(this.PARM_ID, SqlDbType.NVarChar)
+            };
+            array[0].Value = Id;
+            Model.MeterProtocol bizObject = null;
+            IInfo result;
+            try
+            {
+                bool transactionControl = dbInstance.TransactionControl;
+                SqlDataReader sqlDataReader;
+                if (transactionControl)
+                {
+                    sqlDataReader = SqlHelper.ExecuteReader(dbInstance.Transaction, CommandType.StoredProcedure, sQL_GET, array);
+                }
+                else
+                {
+                    sqlDataReader = SqlHelper.ExecuteReader(SqlHelper.MyConnectionString, CommandType.StoredProcedure, sQL_GET, array);
+                }
+                bool hasRows = sqlDataReader.HasRows;
+                if (hasRows)
+                {
+                    sqlDataReader.Read();
+                    this.SetInfo(out bizObject, sqlDataReader);
+                    result = new ReturnInfo
+                    {
+                        BizObject = bizObject,
+                        Code = ErrorEnum.NoError
+                    };
+                }
+                else
+                {
+                    result = new ReturnInfo(ErrorEnum.NoRecord, string.Format("No record found for ID: {0}", Id));
+                }
+            }
+            catch (Exception ex)
+            {
+                result = new ReturnInfo(ErrorEnum.DataException, ex.Message);
+            }
+            return result;
+        }
+        protected IInfo GetRecords(SmartDB dbInstance, string cmdText, SqlParameter[] parms)
+        {
+            var list = new List<Model.MeterProtocol>();
+            IInfo result;
+            try
+            {
+                bool transactionControl = dbInstance.TransactionControl;
+                SqlDataReader sqlDataReader;
+                if (transactionControl)
+                {
+                    bool flag = parms == null;
+                    if (flag)
+                    {
+                        sqlDataReader = SqlHelper.ExecuteReader(dbInstance.Transaction, CommandType.StoredProcedure, cmdText, Array.Empty<SqlParameter>());
+                    }
+                    else
+                    {
+                        sqlDataReader = SqlHelper.ExecuteReader(dbInstance.Transaction, CommandType.StoredProcedure, cmdText, parms);
+                    }
+                }
+                else
+                {
+                    bool flag2 = parms == null;
+                    if (flag2)
+                    {
+                        sqlDataReader = SqlHelper.ExecuteReader(SqlHelper.MyConnectionString, CommandType.StoredProcedure, cmdText, Array.Empty<SqlParameter>());
+                    }
+                    else
+                    {
+                        sqlDataReader = SqlHelper.ExecuteReader(SqlHelper.MyConnectionString, CommandType.StoredProcedure, cmdText, parms);
+                    }
+                }
+                bool hasRows = sqlDataReader.HasRows;
+                if (hasRows)
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        Model.MeterProtocol item;
+                        this.SetInfo(out item, sqlDataReader);
+                        list.Add(item);
+                    }
+                    result = new ReturnInfo
+                    {
+                        BizObject = list,
+                        Code = ErrorEnum.NoError
+                    };
+                }
+                else
+                {
+                    result = new ReturnInfo
+                    {
+                        Message = "No records found",
+                        Code = ErrorEnum.NoRecord
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                result = new ReturnInfo(ErrorEnum.DataException, ex.Message);
+            }
+            return result;
+        }
+        public override IInfo GetRecords(SmartDB dbInstance)
+        {
+            string sQL_GET_LIST = this.SQL_GET_LIST;
+            return this.GetRecords(dbInstance, sQL_GET_LIST, null);
+        }
+        public IInfo GetRecordsByDescription(SmartDB dbInstance, string description)
+        {
+            string cmdText = "[REPORT METERPROTOCOL_LST_BYDESCRIPTION]";
+            SqlParameter[] array = new SqlParameter[]
+            {
+                new SqlParameter("@Description", SqlDbType.NVarChar)
+            };
+            array[0].Value = description;
+            return this.GetRecords(dbInstance, cmdText, array);
+        }
     }
 }
--- TMF.Reports/Model/MeterProtocol.cs
+++ TMF.Reports/Model/MeterProtocol.cs
@@ -1,12 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TMF.Core;
+using TMF.Core.Model;
 
 namespace TMF.Reports.Model
 {
-    class MeterProtocol
+    public class MeterProtocol : ModelInfo
     {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public string EditedBy { get; set; }
+        public DateTime DocDate { get; set; }
+        public int Show { get; set; }
+        public int LockCount { get; set; }
+        public MeterProtocol()
+        {
+            Id = "";
+            base.IsNew = true;
+            base.IsDirty = false;
+            base.IsDeleted = false;
+        }
+        public MeterProtocol(string Id, string Description, 
+                             string CreatedBy, string EditedBy, DateTime DocDate,
+                             int Show, int LockCount)
+        {
+            this.Id = Id;
+            this.Description = Description;
+            this.CreatedBy = CreatedBy;
+            this.EditedBy = EditedBy;
+            this.DocDate = DocDate;
+            this.Show = Show;
+            this.LockCount = LockCount;
+            base.IsNew = false;
+            base.IsDirty = true;
+            base.IsDeleted = false;
+        }
+        protected override object OnGetValue(string fieldname)
+        {
+            switch (fieldname.ToLower())
+            {
+                case "id":
+                    return this.Id;
+                    break;
+                case "description":
+                    return this.Description;
+                    break;
+                case "createdby":
+                    return this.CreatedBy;
+                    break;
+                case "editedby":
+                    return this.EditedBy;
+                    break;
+                case "docdate":
+                    return this.DocDate;
+                    break;
+                case "show":
+                    return this.Show;
+                    break;
+                case "lockcount":
+                    return this.LockCount;
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+        protected override void OnSetValue(string fieldname, object value)
+        {
+            switch (fieldname.ToLower())
+            {
+                case "id":
+                    this.Id = CastDBNull.To<string>(value, "");
+                    break;
+                case "description":
+                    this.Description = CastDBNull.To<string>(value, "");
+                    break;
+                case "createdby":
+                    this.CreatedBy = CastDBNull.To<string>(value, "");
+                    break;
+                case "editedby":
+                    this.EditedBy = CastDBNull.To<string>(value, "");
+                    break;
+                case "docdate":
+                    this.DocDate = CastDBNull.To<DateTime>(value, DateTime.Now);
+                    break;
+                case "show":
+                    this.Show = CastDBNull.To<int>(value, 0);
+                    break;
+                case "lockcount":
+                    this.LockCount = CastDBNull.To<int>(value, 0);
+                    break;
+                default:
+                    //null;
+                    break;
+            }
+        }
     }
 }
